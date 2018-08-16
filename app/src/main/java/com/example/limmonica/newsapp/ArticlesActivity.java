015d9f2f89fb16664d4ba3182010d4e9b266be3b
Monica Muntean <monica@eloquentix.com>
2018-08-17 02:28:41 +0300

    Added AsyncTask, modified QueryUtils to connect to internet, retrieve and parse data

--- app/src/main/java/com/example/limmonica/newsapp/ArticlesActivity.java
+++ app/src/main/java/com/example/limmonica/newsapp/ArticlesActivity.java
@@ -2,6 +2,7 @@ package com.example.limmonica.newsapp;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -9,43 +10,46 @@ import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class ArticlesActivity extends AppCompatActivity {
 
-    public static final String LOG_TAG = ArticlesActivity.class.getName();
+    /**
+     * Sample JSON response for a The Guardian query
+     */
+    private static final String GUARDIAN_REQUEST_URL = "https://content.guardianapis.com/search?from-date=2018-08-01&to-date=2018-08-16&show-fields=byline%2CtrailText&q=uk%2Ftechnology&api-key=a42dcdcf-932d-4091-862e-e4328fa79e1d";
+
+    /**
+     * Adapter for the list of articles
+     */
+    private ArticleAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.article_activity);
 
-        // Create a list of articles
-        ArrayList<Article> articles = QueryUtils.extractArticles();
-//        ArrayList<Article> articles = new ArrayList<>();
-//        articles.add(new Article("2018-08-14T09:53:07Z", "Technology", R.drawable.test_img, "How to turn off Google's location tracking", "Samuel Gibbs", "Turning off location history won’t hide where you are when you use search, Maps or weather. Here’s how to stop being tracked", "https://www.theguardian.com/technology/2018/aug/14/how-to-turn-off-google-location-tracking"));
-//        articles.add(new Article("2018-08-14T09:53:07Z", "Technology", R.drawable.test_img, "How to turn off Google's location tracking", "Samuel Gibbs", "Turning off location history won’t hide where you are when you use search, Maps or weather. Here’s how to stop being tracked", "https://www.theguardian.com/technology/2018/aug/14/how-to-turn-off-google-location-tracking"));
-//        articles.add(new Article("2018-08-14T09:53:07Z", "Technology", R.drawable.test_img, "How to turn off Google's location tracking", "Samuel Gibbs", "Turning off location history won’t hide where you are when you use search, Maps or weather. Here’s how to stop being tracked", "https://www.theguardian.com/technology/2018/aug/14/how-to-turn-off-google-location-tracking"));
-//        articles.add(new Article("2018-08-14T09:53:07Z", "Technology", R.drawable.test_img, "How to turn off Google's location tracking", "Samuel Gibbs", "Turning off location history won’t hide where you are when you use search, Maps or weather. Here’s how to stop being tracked", "https://www.theguardian.com/technology/2018/aug/14/how-to-turn-off-google-location-tracking"));
-//        articles.add(new Article("2018-08-14T09:53:07Z", "Technology", R.drawable.test_img, "How to turn off Google's location tracking", "Samuel Gibbs", "Turning off location history won’t hide where you are when you use search, Maps or weather. Here’s how to stop being tracked", "https://www.theguardian.com/technology/2018/aug/14/how-to-turn-off-google-location-tracking"));
-
         // Find a reference to the {@link ListView in the layout
         ListView articleListView = findViewById(R.id.list);
 
         // Create a new adapter that takes the list of articles as input
-        final ArticleAdapter adapter = new ArticleAdapter(this, articles);
+        mAdapter = new ArticleAdapter(this, new ArrayList<Article>());
 
-        // Set the adapter on the {@link ListView}
-        // so the list can be populated in the user interface
-        articleListView.setAdapter(adapter);
+        // Set the adapter on the {@link ListView} so the list can be populated
+        // in the user interface
+        articleListView.setAdapter(mAdapter);
 
+        // Set an item click listener of the ListView, which sends an intent to a web browser to
+        // open a website with more info about the selected article
         articleListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
 
                 // Find the current article that was clicked on
-                Article currentArticle = adapter.getItem(position);
+                Article currentArticle = mAdapter.getItem(position);
 
                 // Convert the String Url into a Uri object (to pass into the Intent constructor)
+                assert currentArticle != null;
                 Uri articleUri = Uri.parse(currentArticle.getArticleUrl());
 
                 // Create a new intent to view the article Uri
@@ -55,5 +59,58 @@ public class ArticlesActivity extends AppCompatActivity {
                 startActivity(websiteIntent);
             }
         });
+
+        // Start the AsyncTask to fetch the article data
+        ArticleAsyncTask task = new ArticleAsyncTask();
+        task.execute(GUARDIAN_REQUEST_URL);
+    }
+
+    /**
+     * {@link AsyncTask} to perform the network request on a background thread and then update the
+     * UI with the list of articles in the response.
+     */
+    private class ArticleAsyncTask extends AsyncTask<String, Void, List<Article>> {
+
+        /**
+         * This method runs on a background thread and performs the network request and returns a
+         * list of {@link Article}s as the result.
+         *
+         * @param urls are the urls to which the app will connect to get the JSON data
+         * @return a result which is a list of articles
+         */
+        @Override
+        protected List<Article> doInBackground(String... urls) {
+
+            // Don't perform the request if there are no URLs, or if the first URL is null
+            if (urls.length < 1 || urls[0] == null) {
+                return null;
+            }
+
+            // Perform the connection, fetch the data, parse it and store it in a list of articles
+            List<Article> result = QueryUtils.fetchArticleData(urls[0]);
+
+            // Return the list of articles
+            return result;
+        }
+
+        /**
+         * This method receives, as input, the return value from the doInBackground() method. First
+         * we clear out the adapter, to get rid of article data from a previous query to USGS.
+         * Then we update the adapter with the new list of article, which will trigger the
+         * ListView to re-populate its list items.
+         *
+         * @param data is a list of articles from the doInBackground() method
+         */
+        @Override
+        protected void onPostExecute(List<Article> data) {
+
+            // Clear the adapter of previous article data
+            mAdapter.clear();
+
+            // If there is a valid list of {@link Article}s, then add them to the adapter's dataset. This will trigger the ListView to update
+            if (data != null && !data.isEmpty()) {
+                mAdapter.addAll(data);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/limmonica/newsapp/QueryUtils.java
+++ app/src/main/java/com/example/limmonica/newsapp/QueryUtils.java
@@ -1,22 +1,32 @@
 package com.example.limmonica.newsapp;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
- * Helper methods related to requesting and receiving articles data from The Guardian.
+ * Utility class with helper methods related to performing the HTTP request and parsing the article data from The Guardian.
  */
-public class QueryUtils {
+public final class QueryUtils {
 
     /**
-     * Sample JSON response for a The Guardian query
+     * Tag for the log messages
      */
-    private static final String SAMPLE_JSON_RESPONSE = "{\"response\":{\"status\":\"ok\",\"userTier\":\"developer\",\"total\":133,\"startIndex\":1,\"pageSize\":10,\"currentPage\":1,\"pages\":14,\"orderBy\":\"relevance\",\"results\":[{\"id\":\"technology/2018/aug/02/macs-ipods-apps-how-apple-revolutionised-technology\",\"type\":\"article\",\"sectionId\":\"technology\",\"sectionName\":\"Technology\",\"webPublicationDate\":\"2018-08-02T16:03:49Z\",\"webTitle\":\"From Macs to iPods and apps: how Apple revolutionised technology\",\"webUrl\":\"https://www.theguardian.com/technology/2018/aug/02/macs-ipods-apps-how-apple-revolutionised-technology\",\"apiUrl\":\"https://content.guardianapis.com/technology/2018/aug/02/macs-ipods-apps-how-apple-revolutionised-technology\",\"fields\":{\"trailText\":\"Over 42 years, the company has created an ‘app economy’ and placed itself at the centre of it\",\"byline\":\"Alex Hern\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"artanddesign/2018/aug/10/natsiaa-2018-young-guns-breathe-new-life-into-indigenous-art-traditions\",\"type\":\"article\",\"sectionId\":\"artanddesign\",\"sectionName\":\"Art and design\",\"webPublicationDate\":\"2018-08-10T11:00:37Z\",\"webTitle\":\"Natsiaa 2018: young guns breathe new life into Indigenous art traditions\",\"webUrl\":\"https://www.theguardian.com/artanddesign/2018/aug/10/natsiaa-2018-young-guns-breathe-new-life-into-indigenous-art-traditions\",\"apiUrl\":\"https://content.guardianapis.com/artanddesign/2018/aug/10/natsiaa-2018-young-guns-breathe-new-life-into-indigenous-art-traditions\",\"fields\":{\"trailText\":\"This year’s awards in Darwin merge the old and the new with modern materials and technology \",\"byline\":\"Helen Davidson\"},\"isHosted\":false,\"pillarId\":\"pillar/arts\",\"pillarName\":\"Arts\"},{\"id\":\"technology/2018/aug/07/indian-ride-hailing-firm-ola-to-take-on-uber-with-launch-in-uk\",\"type\":\"article\",\"sectionId\":\"technology\",\"sectionName\":\"Technology\",\"webPublicationDate\":\"2018-08-07T11:11:44Z\",\"webTitle\":\"Indian ride-hailing firm Ola to take on Uber with launch in UK\",\"webUrl\":\"https://www.theguardian.com/technology/2018/aug/07/indian-ride-hailing-firm-ola-to-take-on-uber-with-launch-in-uk\",\"apiUrl\":\"https://content.guardianapis.com/technology/2018/aug/07/indian-ride-hailing-firm-ola-to-take-on-uber-with-launch-in-uk\",\"fields\":{\"trailText\":\"Company first in Britain to offer choice of private hire car or black cab through same app\",\"byline\":\"Julia Kollewe\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"technology/2018/aug/02/amazon-halved-uk-corporation-tax-bill-to-45m-last-year\",\"type\":\"article\",\"sectionId\":\"technology\",\"sectionName\":\"Technology\",\"webPublicationDate\":\"2018-08-03T09:05:46Z\",\"webTitle\":\"Amazon halved corporation tax bill despite UK profits tripling\",\"webUrl\":\"https://www.theguardian.com/technology/2018/aug/02/amazon-halved-uk-corporation-tax-bill-to-45m-last-year\",\"apiUrl\":\"https://content.guardianapis.com/technology/2018/aug/02/amazon-halved-uk-corporation-tax-bill-to-45m-last-year\",\"fields\":{\"trailText\":\"Revelation comes after US company posts record $2.5bn profit in most recent quarter\",\"byline\":\"Mark Sweney\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"australia-news/2018/aug/09/crown-resorts-reveals-profits-gained-from-pokies-for-first-time\",\"type\":\"article\",\"sectionId\":\"australia-news\",\"sectionName\":\"Australia news\",\"webPublicationDate\":\"2018-08-09T04:50:51Z\",\"webTitle\":\"Crown windfall driven by $450m in losses from Victorian pokies players\",\"webUrl\":\"https://www.theguardian.com/australia-news/2018/aug/09/crown-resorts-reveals-profits-gained-from-pokies-for-first-time\",\"apiUrl\":\"https://content.guardianapis.com/australia-news/2018/aug/09/crown-resorts-reveals-profits-gained-from-pokies-for-first-time\",\"fields\":{\"trailText\":\"Anti-gambling advocates say profits demonstrate company’s need to expand technology so that problem gamblers can be turned away \",\"byline\":\"Melissa Davey and agencies\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"business/2018/aug/06/more-than-6m-workers-fear-being-replaced-by-machines-report\",\"type\":\"article\",\"sectionId\":\"business\",\"sectionName\":\"Business\",\"webPublicationDate\":\"2018-08-05T23:01:22Z\",\"webTitle\":\"More than 6m workers fear being replaced by machines – report\",\"webUrl\":\"https://www.theguardian.com/business/2018/aug/06/more-than-6m-workers-fear-being-replaced-by-machines-report\",\"apiUrl\":\"https://content.guardianapis.com/business/2018/aug/06/more-than-6m-workers-fear-being-replaced-by-machines-report\",\"fields\":{\"trailText\":\"Government and trade unions urged to do more for those at risk from new technologies \",\"byline\":\"Richard Partington Economics correspondent\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"commentisfree/2018/aug/05/magical-thinking-about-machine-learning-will-not-bring-artificial-intelligence-any-closer\",\"type\":\"article\",\"sectionId\":\"commentisfree\",\"sectionName\":\"Opinion\",\"webPublicationDate\":\"2018-08-05T06:00:03Z\",\"webTitle\":\"Magical thinking about machine learning won’t bring the reality of AI any closer | John Naughton\",\"webUrl\":\"https://www.theguardian.com/commentisfree/2018/aug/05/magical-thinking-about-machine-learning-will-not-bring-artificial-intelligence-any-closer\",\"apiUrl\":\"https://content.guardianapis.com/commentisfree/2018/aug/05/magical-thinking-about-machine-learning-will-not-bring-artificial-intelligence-any-closer\",\"fields\":{\"trailText\":\"Unchecked flaws in algorithms, and even the technology itself, should put a brake on the escalating use of big data\",\"byline\":\"John Naughton\"},\"isHosted\":false,\"pillarId\":\"pillar/opinion\",\"pillarName\":\"Opinion\"},{\"id\":\"media/2018/aug/02/fifth-of-britons-feel-stressed-if-they-cant-access-internet-ofcom-report\",\"type\":\"article\",\"sectionId\":\"media\",\"sectionName\":\"Media\",\"webPublicationDate\":\"2018-08-02T06:14:21Z\",\"webTitle\":\"Britons spend average of 24 hours a week online, Ofcom says\",\"webUrl\":\"https://www.theguardian.com/media/2018/aug/02/fifth-of-britons-feel-stressed-if-they-cant-access-internet-ofcom-report\",\"apiUrl\":\"https://content.guardianapis.com/media/2018/aug/02/fifth-of-britons-feel-stressed-if-they-cant-access-internet-ofcom-report\",\"fields\":{\"trailText\":\"Study reveals dramatic rise in addiction to technology, as average Briton checks a mobile phone every 12 minutes \",\"byline\":\"Jim Waterson Media editor\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"politics/2018/jul/31/uk-will-act-alone-against-tech-giant-tax-avoidance-if-global-solution-falters\",\"type\":\"article\",\"sectionId\":\"politics\",\"sectionName\":\"Politics\",\"webPublicationDate\":\"2018-08-01T10:47:03Z\",\"webTitle\":\"UK will act alone against tech firm tax avoidance if global solution falters\",\"webUrl\":\"https://www.theguardian.com/politics/2018/jul/31/uk-will-act-alone-against-tech-giant-tax-avoidance-if-global-solution-falters\",\"apiUrl\":\"https://content.guardianapis.com/politics/2018/jul/31/uk-will-act-alone-against-tech-giant-tax-avoidance-if-global-solution-falters\",\"fields\":{\"trailText\":\"Exclusive: Treasury minister Mel Stride suggests digital revenue levy on Google and Facebook\",\"byline\":\"Pippa Crerar\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"},{\"id\":\"education/2018/aug/05/john-prest-obituary\",\"type\":\"article\",\"sectionId\":\"education\",\"sectionName\":\"Education\",\"webPublicationDate\":\"2018-08-05T14:20:26Z\",\"webTitle\":\"John Prest obituary\",\"webUrl\":\"https://www.theguardian.com/education/2018/aug/05/john-prest-obituary\",\"apiUrl\":\"https://content.guardianapis.com/education/2018/aug/05/john-prest-obituary\",\"fields\":{\"trailText\":\"Other lives:\",\"byline\":\"Jeremy Burchardt\"},\"isHosted\":false,\"pillarId\":\"pillar/news\",\"pillarName\":\"News\"}]}}";
+    private static final String LOG_TAG = QueryUtils.class.getSimpleName();
 
     /**
      * Create a private constructor because no one should ever create a {@link QueryUtils} object.
@@ -27,71 +37,235 @@ public class QueryUtils {
     }
 
     /**
+     * Returns a new URL Object from a given string URL
+     */
+    private static URL createUrl(String stringUrl) {
+
+        // Initialize the url to null
+        URL url = null;
+
+        // Try to update the variable and if not successful catch the exception
+        try {
+            url = new URL(stringUrl);
+        } catch (MalformedURLException e) {
+            Log.e(LOG_TAG, "Error with creating URL", e);
+        }
+
+        // Return the URL Object
+        return url;
+    }
+
+    /**
+     * Make a HTTP request to the given URL and return a String as the response
+     */
+    private static String makeHttpRequest(URL url) throws IOException {
+
+        // Initialize the response
+        String jsonResponse = "";
+
+        // If the Url is null, then return early
+        if (url == null) {
+            return jsonResponse;
+        }
+
+        // Initialize the url connection
+        HttpURLConnection urlConnection = null;
+
+        // Initialize the input stream
+        InputStream inputStream = null;
+
+        // Try to establish a connection
+        try {
+            // Open the connection
+            urlConnection = (HttpURLConnection) url.openConnection();
+
+            // Set a read timeout to 10 seconds
+            urlConnection.setReadTimeout(10000);
+
+            // Set a timeout for the connection to 15 seconds
+            urlConnection.setConnectTimeout(15000);
+
+            // Set the request method, for when connected to the url, to get/read the data
+            urlConnection.setRequestMethod("GET");
+
+            // Connect tot the url
+            urlConnection.connect();
+
+            // If the connection was successful then read the input stream and parse the response
+            if (urlConnection.getResponseCode() == HttpURLConnection.HTTP_OK) {
+
+                // Read the input stream
+                inputStream = urlConnection.getInputStream();
+
+                // Parse the response from input stream and store it into a String
+                jsonResponse = readFromStream(inputStream);
+
+            } else {
+
+                // Otherwise log the error message and the connection response code
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+
+        } catch (IOException e) {
+            // Catch the IOException error if there are problems with connecting
+            Log.e(LOG_TAG, "Problem retrieving the articles JSON results.", e);
+
+        } finally {
+
+            // If the connection is not null
+            if (urlConnection != null) {
+
+                // Disconnect
+                urlConnection.disconnect();
+            }
+
+            // If there was an input stream, closing it could throw a IOException which is why it
+            // is specified in the method signature
+            if (inputStream != null) {
+
+                // Close it
+                inputStream.close();
+
+            }
+        }
+
+        // Return the response from reading the JSON data in a String
+        return jsonResponse;
+    }
+
+    /**
+     * Convert the {@link InputStream} into a String which contains the whole JSON response from
+     * the server
+     */
+    private static String readFromStream(InputStream inputStream) throws IOException {
+
+        // Create a new String builder object
+        StringBuilder output = new StringBuilder();
+
+        // If the input stream is not null
+        if (inputStream != null) {
+
+            // Create a new input stream reader object based on the input stream
+            InputStreamReader inputStreamReader = new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+
+            // Create a new buffered reader object from the input stream reader
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+
+            // Read and store into the buffer the first line
+            String line = reader.readLine();
+
+            // While the line is not null
+            while (line != null) {
+                // Append it to the output string builder
+                output.append(line);
+                // Read and store into the buffer the next line
+                line = reader.readLine();
+            }
+        }
+
+        // Return the output
+        return output.toString();
+    }
+
+    /**
      * Return a list of {@link Article} objects that has been built up from
      * parsing a JSON response.
      */
-    public static ArrayList<Article> extractArticles() {
+    private static List<Article> extractResponseFromJson(String articleJSON) {
+
+        // If the JSON string is empty or null, then return early
+        if (TextUtils.isEmpty(articleJSON)) {
+            return null;
+        }
 
         // Create an empty ArrayList that we can start adding articles to
-        ArrayList<Article> articles = new ArrayList<>();
+        List<Article> articles = new ArrayList<>();
 
-        // Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
+        // Try to parse the data read from The Guardian. If there's a problem with the way the JSON
         // is formatted, a JSONException exception object will be thrown.
-        // Catch the exception so the app doesn't crash, and print the error message to the logs.
         try {
 
-            // Parse the response given by the SAMPLE_JSON_RESPONSE string and build up a list of Article objects with the corresponding data.
-            //        Convert SAMPLE_JSON_RESPONSE String into a JSONObject / Create a JSONObject from the SAMPLE_JSON_RESPONSE string
-            JSONObject baseJsonResponse = new JSONObject(SAMPLE_JSON_RESPONSE);
+            // Parse the response and build up a list of Article objects with the corresponding data:
+            // Create a JSONObject from the data received
+            JSONObject baseJsonResponse = new JSONObject(articleJSON);
 
-            //        Extract "response" JSONObject
+            // Extract "response" JSONObject
             JSONObject responseObject = baseJsonResponse.getJSONObject("response");
 
-            //        Extract “results” JSONArray / Extract the JSONArray associated with the key called "results", which represents a list of results (or articles).
+            // Extract the JSONArray associated with the key called "results", which represents a
+            // list of results (or articles).
             JSONArray articleArray = responseObject.getJSONArray("results");
 
-            //        Loop through each result in the array / For each article in the articleArray, create an {@link Article} object
+            // For each article in the articleArray, create an {@link Article} object
             for (int i = 0; i < articleArray.length(); i++) {
 
-                //        Get article JSONObject at position i
+                // Get a single article JSONObject at position i within the list of articles
                 JSONObject currentArticle = articleArray.getJSONObject(i);
 
-                //        Extract "webPublicationDate" for date
+                // Extract the value for the key called "webPublicationDate"
                 String date = currentArticle.getString("webPublicationDate");
 
-                //        Extract "sectionName" for section
+                // Extract the value for the key called "sectionName"
                 String section = currentArticle.getString("sectionName");
 
-                //        Get "fields" JSONObject
-                JSONObject fields = currentArticle.getJSONObject("fields");
-
-                //        Extract "webTitle" for title
+                // Extract the value for the key called "webTitle"
                 String title = currentArticle.getString("webTitle");
 
-                //        Extract "byline" for author
+                // For a given article, extract the JSONObject associated with the key called
+                // "fields", which represents a list of additional fields for that article
+                JSONObject fields = currentArticle.getJSONObject("fields");
+
+                // Extract the value for the key called "byline"
                 String author = fields.getString("byline");
 
-                //        Extract "trailText" for trail
+                // Extract the value for the key called "trailText"
                 String trail = fields.getString("trailText");
 
-                //        Extract "webUrl" for url
+                // Extract the value for the key called "webUrl"
                 String url = currentArticle.getString("webUrl");
 
-                //        14) Create Article java object from: date, section, title, author, trail, and url
+                // Create Article java object from: date, section, title, author, trail, and url
                 Article article = new Article(date, section, title, author, trail, url);
 
-                //        15) Add article to list of articles
+                // Add article to list of articles
                 articles.add(article);
             }
 
         } catch (JSONException e) {
-            // If an error is thrown when executing any of the above statements in the "try" block,
-            // catch the exception here, so the app doesn't crash. Print a log message
-            // with the message from the exception.
+
+            // If an error is thrown when executing any of the above statements in the "try" block
+            // print a log message with the message from the exception.
             Log.e("QueryUtils", "Problem parsing the article JSON results", e);
         }
 
         // Return the list of articles
+        return articles;
+    }
+
+    /**
+     * Query the USGS dataset and return a list of {@link Article} objects.
+     */
+    public static List<Article> fetchArticleData(String requestUrl) {
+
+        // Create a URL Object
+        URL url = createUrl(requestUrl);
+
+        // Perform HTTP request to the URL and receive a JSON response back
+        String jsonResponse = null;
+
+        try {
+
+            jsonResponse = makeHttpRequest(url);
+
+        } catch (IOException e) {
+
+            Log.e(LOG_TAG, "Problem making the HTTP request", e);
+        }
+
+        // Extract relevant fields from the JSON response and create a list of {@link Article}s.
+        List<Article> articles = extractResponseFromJson(jsonResponse);
+
+        // Return the list of {@link Article}s
         return articles;
     }
 }
